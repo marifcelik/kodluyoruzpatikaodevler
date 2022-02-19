3270de7bc2140b48f6e4f91edda76e6cf2c6d6d5
arif <arif-celik56@hotmail.com>
2022-02-19 13:32:02 +0300

    try eklendi

---
+++ c#101/try.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("sayı : ");
+try
+{
+    int i = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("girdiğiniz sayı : " + i);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("hata : " + ex.Message.ToString());
+}
+finally
+{
+    Console.WriteLine("finally çalıştı");
+}
+
+//try 2
+try
+{
+    // int a = Int32.Parse(null); //null
+    // int b = int.Parse("deneme"); //format
+    byte c = byte.Parse("256"); //overflow
+}
+catch(ArgumentNullException ex)
+{
+    Console.WriteLine("hata a : ");
+    Console.WriteLine(ex);
+}
+catch(FormatException ex)
+{
+    Console.WriteLine("hata : ");
+    Console.WriteLine(ex);
+}
+catch(OverflowException ex)
+{
+    Console.WriteLine("hata : ");
+    Console.WriteLine(ex);
+}
+finally
+{
+    Console.WriteLine("ikinci finally çalıştı");
+}
