9a25bebb9cc27a2e47111dd4590fe0a537de8d66
arif <arif-celik56@hotmail.com>
2022-02-19 21:51:31 +0300

    if ternary

---
+++ c#101/if-ternary.cs
@@ -0,0 +1,13 @@
+﻿int time = DateTime.Now.Hour;
+
+if (time >= 6 && time < 11)
+    Console.WriteLine("günaydın");
+else if (time <= 18)
+    Console.WriteLine("iyi günler");
+else
+    Console.WriteLine("iyi akşamlar");
+
+// string mesaj = time <= 18 ? "iyi günler" : "iyi akşamlar";
+
+string mesaj = time >= 6 && time < 11 ? "günaydın" : time <= 18 ? "iyi günler" : "iyi akşamlar";
+Console.WriteLine(mesaj);
