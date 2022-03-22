94c36e478658e0dcdaa164015569eea2eef86e12
arif <arif-celik56@hotmail.com>
2022-03-22 14:48:52 +0300

    ödev 1

---
+++ js/jsodev1/script.js
@@ -0,0 +1,16 @@
+setInterval(() => {
+    let date = new Date();
+    let saat = date.getHours(),
+        dakika = date.getMinutes(),
+        saniye = date.getSeconds()
+
+    document.querySelector('.hour').textContent = saat;
+    document.querySelector('.minute').textContent = dakika;
+    saniye < 10 ? document.querySelector('.second').textContent = '0' + saniye : document.querySelector('.second').textContent = saniye;
+}, 1000)
+
+let ad = prompt('adınız ?')
+if (!ad)
+    ad = 'anonim'
+document.querySelector('h1').textContent = `merhaba ${ad[0].toLocaleUpperCase() + ad.slice(1, ad.length).toLocaleLowerCase()}, hoşgeldin.`
+
